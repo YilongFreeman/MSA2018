d0869a0474ec213a2cc693563dac8d824fa22adb
YilongFreeman <02:1f:30:3f:f9:89:c5:98:9e:d2:95:18:00:30:5a:84leeddeng@hotmail.com>
2018-08-29 20:10:05 +1200

    Commit message

--- Weather/weather/src/App.tsx
+++ Weather/weather/src/App.tsx
@@ -6,17 +6,43 @@ import Form from './Component/Form';
 
 import Weather from './Component/Weather';
 
+const apiKey = "5171068db009ea90ef470f2bf369a577";
 
-class App extends React.Component {
+interface IApp {
+  result: any[]
+}
+
+class App extends React.Component<{}, IApp> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      result: []
+    }
+    
+    this.callAPI = this.callAPI.bind(this);
+  }
   public render() {
     return (
       <div> 
         <Titles/>
         <Form/>
         <Weather/>
+        <input onChange={this.callAPI}/>
+        <button value="London" onClick={this.callAPI}>London</button>
+        {JSON.stringify(this.state.result)}
         </div>
     )
   }
+
+  public async callAPI(e:any) {
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+e.target.value+"&appid="+apiKey;
+    const api = await fetch(url);
+    const response = await api.json();
+    if (response !== undefined) {
+      this.setState({result: response});
+    }
+  }
+
 };
 
 export default App;
